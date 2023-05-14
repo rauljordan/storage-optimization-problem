195023d2392a7a14c68e77bce7d5245f0db5ebc6
rauljordan <raul@prysmaticlabs.com>
2023-05-14 10:09:45 -0400

    karlin dist

---
+++ src/karlin.rs
@@ -0,0 +1,36 @@
+use rand::{thread_rng, Rng};
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn check_karlin_ev() {
+        let cost = 1;
+        let res = pdf(cost, cost);
+        assert_eq!(format!("{}", res), "1.58");
+    }
+}
+
+/// Parametrized by c, creates a pdf with an expected value of (1 / ((e - 1) * C)).
+pub fn pdf(t: u64, c: u64) -> f64 {
+    let e = std::f64::consts::E;
+    let lhs = 1.0 / ((e - 1.0) * c as f64);
+    let rhs = e.powf(t as f64 / c as f64);
+    lhs * rhs
+}
+
+/// Monte carlo sampling method for the karlin pdf.
+pub fn sample(cost: u64) -> u64 {
+    let max_iters = 10_000;
+    let mut rng = thread_rng();
+    let max_value: f64 = pdf(cost, cost);
+    for _ in 0..max_iters {
+        let rand_x = rng.gen_range(0..=cost);
+        let rand_y = max_value * rng.gen_range(0.0f64..1.0f64);
+        let calc_y = pdf(rand_x, cost);
+        if rand_y <= calc_y {
+            return rand_x;
+        }
+    }
+    panic!("could not find through {} iterations", max_iters)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,54 @@
+use ordered_float::NotNan;
+use rand::{thread_rng, Rng};
+use std::iter::repeat;
 use std::iter::Peekable;
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    use rand::{thread_rng, Rng};
-    use std::iter::repeat;
-    fn generate_access_list(len: usize, num_ticks: u64) -> Vec<u64> {
-        let mut rng = thread_rng();
-        let mut access_list: Vec<u64> = repeat(0)
-            .take(len)
-            .map(|_: u64| rng.gen_range(0..num_ticks as u64))
-            .collect();
-        access_list.sort();
-        access_list.dedup();
-        access_list
-    }
-    fn karlin_pdf(t: u64, c: u64) -> f64 {
-        let e = std::f64::consts::E;
-        let lhs = 1.0 / ((e - 1.0) * c as f64);
-        let rhs = e.powf(t as f64 / c as f64);
-        lhs * rhs
-    }
+mod karlin;
 
-    // TODO: Fix up box method.
-    fn sample_karlin(max_iters: u64) -> u64 {
-        let mut rng = rand::thread_rng();
-        let max_value: f64 = 1.58;
-        for _ in 0..max_iters {
-            let rand_x = rng.gen_range(0..40);
-            let rand_y = max_value * rng.gen_range(0.0f64..=1.0f64);
-            let calc_y = karlin_pdf(rand_x, 1);
-            println!("randx={}, randy={}, calcy={}", rand_x, rand_y, calc_y);
-            if rand_y <= calc_y {
-                return rand_x;
-            }
-        }
-        panic!("could not find in iterations")
-    }
-    #[test]
-    fn check_karlin_ev() {
-        let res = karlin_pdf(1, 1);
-        assert_eq!(format!("{:.2}", res), "1.58");
-    }
-}
-
+/// We show the 2-competitive strategy for a deterministic
+/// approach will never have a competitive ratio worse than 2x
+/// the offline algorithm for a variety of access list sizes and random
+/// access lists.
 fn main() {
     let keep_cost = 1u64;
-    let recover_cost = 5u64;
-    let num_ticks = 25;
-    let access_list = vec![7, 13, 19, 25];
-    let (online_cost, offline_cost, ratio) =
-        compare(keep_cost, recover_cost, access_list, num_ticks);
+    let recover_cost = 10u64;
+    let max_access_list_size = 50;
+    let num_iters = 1000;
+    let mut max_ratios = vec![];
+    for i in 1..max_access_list_size {
+        let mut ratios = vec![];
+        for _ in 0..num_iters {
+            let access_list = generate_access_list(i, max_access_list_size as u64);
+            let num_ticks = access_list.last().unwrap().clone();
+            let competitive_ratio =
+                compare_deterministic_approach(keep_cost, recover_cost, access_list, num_ticks);
+            ratios.push(competitive_ratio);
+        }
+        let max_val = ratios
+            .into_iter()
+            .map(NotNan::new)
+            .filter_map(Result::ok)
+            .max()
+            .unwrap();
+        println!(
+            "access_list_size={}, iters={}, max_competitive_ratio={}",
+            i, num_iters, *max_val,
+        );
+        max_ratios.push(max_val);
+    }
+    let max_ratio = max_ratios.into_iter().max().unwrap();
     println!(
-        "online_cost={}, offline_cost={}, competitive_ratio={:.2}",
-        online_cost, offline_cost, ratio
+        "max_competitive_ratio={} across all attempts and access list sizes",
+        *max_ratio,
     );
 }
 
-fn compare(
+fn compare_deterministic_approach(
     keep_cost: u64,
     recover_cost: u64,
     access_list: Vec<u64>,
     num_ticks: u64,
-) -> (u64, u64, f64) {
+) -> f64 {
     // Offline, omniscient instance.
     let offline = OfflineInstance::new(
         keep_cost,
@@ -75,18 +61,46 @@ fn compare(
     }
     let offline_cost = sim.node.accrued_cost;
 
-    // Naive, 2-competitive online instance.
+    // Standard, 2-competitive online instance.
     let online = NaiveInstance::new(keep_cost, recover_cost);
     let mut sim = Simulator::new(access_list, online);
     for _ in 0..num_ticks {
         sim.tick();
     }
     let online_cost = sim.node.accrued_cost;
-    (
-        online_cost,
-        offline_cost,
-        online_cost as f64 / offline_cost as f64,
-    )
+
+    // Competitive ratio.
+    online_cost as f64 / offline_cost as f64
+}
+
+fn compare_randomized_approach(
+    keep_cost: u64,
+    recover_cost: u64,
+    access_list: Vec<u64>,
+    num_ticks: u64,
+) -> f64 {
+    // Offline, omniscient instance.
+    let offline = OfflineInstance::new(
+        keep_cost,
+        recover_cost,
+        access_list.clone().into_iter().peekable(),
+    );
+    let mut sim = Simulator::new(access_list.clone(), offline);
+    for _ in 0..num_ticks {
+        sim.tick();
+    }
+    let offline_cost = sim.node.accrued_cost;
+
+    // Randomized, karlin online approach.
+    let online = KarlinInstance::new(keep_cost, recover_cost);
+    let mut sim = Simulator::new(access_list, online);
+    for _ in 0..num_ticks {
+        sim.tick();
+    }
+    let online_cost = sim.node.accrued_cost;
+
+    // Competitive ratio.
+    online_cost as f64 / offline_cost as f64
 }
 
 #[derive(Debug, Clone)]
@@ -158,10 +172,6 @@ where
             (Policy::Keep, Some(&elem)) => {
                 let time_to_next_access = elem - self.t;
                 if time_to_next_access >= self.recover_cost {
-                    println!(
-                        "Offline: Discarding time={}, total={}",
-                        self.t, self.accrued_cost
-                    );
                     self.policy = Policy::Discard;
                 }
             }
@@ -169,10 +179,6 @@ where
         }
         if matches!(self.policy, Policy::Keep) {
             self.accrued_cost += self.keep_cost;
-            println!(
-                "Offline: Keeping time={}, total={}",
-                self.t, self.accrued_cost
-            );
         }
         if !access {
             return;
@@ -182,10 +188,6 @@ where
         if matches!(self.policy, Policy::Discard) {
             self.accrued_cost += self.recover_cost;
             self.policy = Policy::Keep;
-            println!(
-                "Offline: Recovering time={}, total={}",
-                self.t, self.accrued_cost
-            );
         }
     }
 }
@@ -220,18 +222,10 @@ impl Algorithm for NaiveInstance {
         // is >= recover cost, then we should discard.
         let should_discard = (self.t - self.last_access) >= self.recover_cost;
         if matches!(self.policy, Policy::Keep) && should_discard {
-            println!(
-                "Online: Discarding time={}, total={}",
-                self.t, self.accrued_cost
-            );
             self.policy = Policy::Discard;
         }
         if matches!(self.policy, Policy::Keep) {
             self.accrued_cost += self.keep_cost;
-            println!(
-                "Online: Keeping time={}, total={}",
-                self.t, self.accrued_cost
-            );
         }
         if !access {
             return;
@@ -242,10 +236,68 @@ impl Algorithm for NaiveInstance {
         if matches!(self.policy, Policy::Discard) {
             self.accrued_cost += self.recover_cost;
             self.policy = Policy::Keep;
-            println!(
-                "Online: Recovering time={}, total={}",
-                self.t, self.accrued_cost
-            );
         }
     }
+}
+
+#[derive(Debug, Clone)]
+struct KarlinInstance {
+    t: u64,
+    keep_cost: u64,
+    recover_cost: u64,
+    policy: Policy,
+    accrued_cost: u64,
+    last_access: u64,
+}
+
+impl KarlinInstance {
+    fn new(keep_cost: u64, recover_cost: u64) -> Self {
+        Self {
+            t: 0,
+            last_access: 0,
+            keep_cost,
+            recover_cost,
+            policy: Policy::Keep,
+            accrued_cost: 0,
+        }
+    }
+}
+
+impl Algorithm for KarlinInstance {
+    fn tick(&mut self, access: bool) {
+        self.t += 1;
+        // 2-competitive algorithm. If time since last access
+        // is >= D, then we should discard where D is sampled from a Karlin
+        // distribution.
+        let sample = karlin::sample(self.recover_cost);
+        let time_elapsed = self.t - self.last_access;
+        let should_discard = time_elapsed >= sample;
+        if matches!(self.policy, Policy::Keep) && should_discard {
+            self.policy = Policy::Discard;
+        }
+        if matches!(self.policy, Policy::Keep) {
+            self.accrued_cost += self.keep_cost;
+        }
+        if !access {
+            return;
+        }
+        self.last_access = self.t;
+
+        // Incur a recovery cost if necessary.
+        if matches!(self.policy, Policy::Discard) {
+            self.accrued_cost += self.recover_cost;
+            self.policy = Policy::Keep;
+        }
+    }
+}
+
+fn generate_access_list(len: usize, max: u64) -> Vec<u64> {
+    let mut rng = thread_rng();
+    let mut access_list: Vec<u64> = repeat(0)
+        .take(len)
+        .map(|_: u64| rng.gen_range(1..=max as u64))
+        .collect();
+    access_list.sort();
+    access_list.dedup();
+    access_list
 }
